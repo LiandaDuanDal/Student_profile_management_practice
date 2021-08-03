a10ce99344d8ec1145d961b09bfd97a80ccd6912
Lianda_Duan_Dal <LiandaDuanDal@users.noreply.github.com>
2021-08-03 13:53:17 -0300

    first time to commit from the ipad

--- student/model/connect.js
+++ student/model/connect.js
@@ -1,6 +1,8 @@
 // import mongoose databse module
 const mogoose = require('mongoose');
 // connect to the database
+// then---execute for successful connect
+// catch--wxwcute when fail to connect to the server
 mogoose.connect("mongodb://localhost/playground")
     .then(() => { console.log("@@@@@Connected to the databse@@@@@") })
     .catch(() => { console.log("*****failed to connect to the database*****") });
