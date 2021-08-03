68704bd90c2a30976c5c01fd04ed44976eca6ad1
Lianda_Duan_Dal <LiandaDuanDal@users.noreply.github.com>
2021-08-03 13:55:52 -0300

    second commit from ipad

--- student/model/connect.js
+++ student/model/connect.js
@@ -2,7 +2,7 @@
 const mogoose = require('mongoose');
 // connect to the database
 // then---execute for successful connect
-// catch--wxwcute when fail to connect to the server
+// catch--execute when fail to connect to the server
 mogoose.connect("mongodb://localhost/playground")
     .then(() => { console.log("@@@@@Connected to the databse@@@@@") })
     .catch(() => { console.log("*****failed to connect to the database*****") });
